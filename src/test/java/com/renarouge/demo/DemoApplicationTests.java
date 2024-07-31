679d780c4a5672f90bb0dce7b91f1667b261efb4
Jéssica Santos <jessica.js1306@gmail.com>
2024-07-31 17:56:48 -0300

    Spring Boot Rest Controller

---
+++ src/test/java/com/renarouge/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renarouge.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
