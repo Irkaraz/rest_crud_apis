1aad7eab6c9cbf8ebadfac451c6667beb9847a12
Jéssica Santos <jessica.js1306@gmail.com>
2024-08-14 18:22:19 -0300

    Spring Boot REST Exception Handling

--- src/main/java/com/renarouge/demo/rest/StudentRestController.java
+++ src/main/java/com/renarouge/demo/rest/StudentRestController.java
@@ -1,9 +1,10 @@
 package com.renarouge.demo.rest;
 
 import com.renarouge.demo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,16 +13,39 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return a list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load the student data ... only once!
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Jéssica", "Santos"));
         theStudents.add(new Student("Leandro", "Leite"));
         theStudents.add(new Student("Isabelly", "Aparecida"));
+    }
+
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
 
         return theStudents;
     }
 
+    // define endpoint or "student/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // check the studentId again list size
+        if ((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
+        return theStudents.get(studentId);
+
+    }
+
 }
+
+
