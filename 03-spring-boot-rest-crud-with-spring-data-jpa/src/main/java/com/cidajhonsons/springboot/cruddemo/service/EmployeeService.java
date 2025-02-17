c648971c2632ef937a668a6b711239b4f7e94e5c
Jéssica Santos <jessica.js1306@gmail.com>
2025-02-17 16:17:35 -0300

    Spring Boot REST: Spring Data JPA

---
+++ 03-spring-boot-rest-crud-with-spring-data-jpa/src/main/java/com/cidajhonsons/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.cidajhonsons.springboot.cruddemo.service;
+
+import com.cidajhonsons.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
---
+++ 03-spring-boot-rest-crud-with-spring-data-jpa/src/test/java/com/cidajhonsons/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cidajhonsons.springboot.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
