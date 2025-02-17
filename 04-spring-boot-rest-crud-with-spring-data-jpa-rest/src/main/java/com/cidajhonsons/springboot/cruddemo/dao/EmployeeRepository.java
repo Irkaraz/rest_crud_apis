0054a773914cc3e989ae61d2308b6a41075cb919
Jéssica Santos <jessica.js1306@gmail.com>
2025-02-17 17:39:04 -0300

    Spring Data REST Configs

--- 04-spring-boot-rest-crud-with-spring-data-jpa-rest/src/main/java/com/cidajhonsons/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud-with-spring-data-jpa-rest/src/main/java/com/cidajhonsons/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.cidajhonsons.springboot.cruddemo.dao;
 
 import com.cidajhonsons.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository <Employee, Integer>{
 
     // é isso ... não precisa mais escrever nenhum código kakakakak
